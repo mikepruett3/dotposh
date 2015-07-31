b66dbfe90eeb89b429d2294678f1c177b64ac719
Mike Pruett <mikepruett3@gmail.com>
2015-07-31 14:28:13 -0500

    Added SQLite Module

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # Installation:
 
+From Command Line
+
     git clone https://github.com/mikepruett3/dotposh.git %UserProfile%\dotposh
+
+From PowerShell
+
+    git clone https://github.com/mikepruett3/dotposh.git $Env:UserProfile\dotposh
 
 ## Create symlinks:
 
@@ -9,4 +15,18 @@
     mklink %UserProfile%\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 %UserProfile%\dotposh\profile.ps1
     mklink /D %UserProfile%\Documents\WindowsPowerShell\modules\ %UserProfile%\dotposh\modules
 
-## More...
+## Installing SubModules:
+
+Switch into the "%UserProfile%\dotposh" directory, and fetch submodules
+
+    cd %UserProfile%\dotposh
+    git submodule init
+    git submodule update
+
+## Adding New SubModules:
+
+Adding new submodules to the directory
+
+    cd %UserProfile%\dotposh
+    git submodule add http://github.com/<username>/<repository>.git modules/<module-name>
+
--- profile.ps1
+++ profile.ps1
@@ -35,13 +35,35 @@ Function work-history {
     $history | select -Unique | Convertfrom-csv -errorAction SilentlyContinue | Add-History -errorAction SilentlyContinue
 }
 
+# Get-RedirectedUrl Function - http://stackoverflow.com/questions/25125818/powershell-invoke-webrequest-how-to-automatically-use-original-file-name
+Function Get-DownloadFileName {
+    Param (
+        [Parameter(Mandatory=$true)]
+        [String]$URL
+    )
+    $request = Invoke-WebRequest $url | Select -ExpandProperty Headers
+    $equalpos = $request.Get_Item("Content-Disposition").IndexOf("=")
+    return $request.Get_Item("Content-Disposition").SubString($equalpos + 1)
+}
+
+Function Get-DownloadFile {
+    Param (
+        [Parameter(Mandatory=$true)]
+        [String]$URL
+    )
+    $filename = Get-DownloadFileName $url
+    return Invoke-WebRequest $url -OutFile $filename
+}
+
+# Overwrite Existing WGET Alias
+Set-Alias -Name wget -Value Get-DownloadFile -Option AllScope
+
 # Alias definitions
 New-Alias -Name which -Value "Get-Command" -Description "Alias for Get-Command, mimic's Linux which command"
 New-Alias -Name grep -Value "Select-String" -Description "Alias for Select-String, mimic's Linux grep command"
 New-Alias -Name ll -Value "Get-ChildItem" -Description "Alias for Get-ChildItem, like 'ls' from linux."
 New-Alias -Name l -Value "Get-ChildItem" -Description "Alias for Get-ChildItem, like 'ls' from linux."
 
-#
 #Call Work-History Function
 work-history
 
