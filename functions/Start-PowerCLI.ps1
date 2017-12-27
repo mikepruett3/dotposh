3a8d28505dd81b25e7febcbc9fa48dd604ee5271
Mike Pruett <mikepruett3@gmail.com>
2017-12-27 15:14:23 -0600

    Added new Start-PowerCLI Function

---
+++ functions/Start-PowerCLI.ps1
@@ -0,0 +1,26 @@
+function Start-PowerCLI {
+    <#
+    .SYNOPSIS
+        Function locates and dot-sources the correct PowerCLI Environment into running shell
+    .DESCRIPTION
+        Function tests to ensure that the PowerCLI PSSnapins and Environment Scripts are located, and the imports them into the running shell
+    .EXAMPLE
+        PS > Start-PowerCLI
+        Tries to import the PowerCLI PSSnapin and Environment Script from the Defaults
+    .NOTES
+        Author: Mike Pruett
+        Date: December 27th, 2017
+    #>
+    If ( ! (Get-PSSnapin VMware.VimAutomation.Core )) {
+        Add-PSSnapin VMware.VimAutomation.Core
+    } else {
+        Write-Error "Cannot find VMware.VimAutomation.Core PSSnapin!!"
+        Break
+    }
+    $PowerCLIEnv = "$Env:ProgramFiles(x86)\VMware\Infrastructure\PowerCLI\Scripts\Initialize-PowerCLIEnvironment.ps1"
+    if (Test-Path $PowerCLIEnv) {
+        . "$PowerCLIEnv"
+    } else {
+        Write-Error "Cannot find PowerCLI Environment Script!!!"
+    }
+}
