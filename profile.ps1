8b9b9aa892e7fdf3ac6387f7d6834e29c8618a57
Mike Pruett <mikepruett3@gmail.com>
2018-12-21 15:33:59 -0600

    Trying something else

--- profile.ps1
+++ profile.ps1
@@ -77,7 +77,7 @@ Function ls($path) { Get-ChildItem -name -force $path }
 Function ll($path) { Get-ChildItem -force $path }
 Function hc { Get-History -count $MaximumHistoryCount }
 Function ep { gvim $Profile }
-Function de { "$Env:UserProfile\scoop\apps\docker-machine\current\docker-machine.exe" env | Invoke-Expression }
+# Function de { "$Env:UserProfile\scoop\apps\docker-machine\current\docker-machine.exe" env | Invoke-Expression }
 
 # Alias definitions
 Set-Alias grep      Select-String
@@ -95,6 +95,8 @@ Set-Alias exch      Start-ExchEnv
 
 #Call Work-History Function
 work-history
+
+& "$Env:UserProfile\scoop\apps\docker-machine\current\docker-machine.exe" env | Invoke-Expression
 
 # http://winterdom.com/2008/08/mypowershellprompt
 function shorten-path([string] $path) { 
