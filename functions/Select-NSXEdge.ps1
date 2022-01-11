27babc616435c8e61eb345a5ab6d77cb53c51cce
Mike Pruett <mikepruett3@gmail.com>
2022-01-11 16:19:05 -0600

    More Select-NSXEdge function changes

--- functions/Select-NSXEdge.ps1
+++ functions/Select-NSXEdge.ps1
@@ -2,11 +2,11 @@
 function Select-NSXEdge {
     param ()
     if (!($NSXEdge)) {
-        Clear-Variable -Name "NSXEdge" -Scope Local -ErrorAction SilentlyContinue
+        Clear-Variable -Name "NSXEdge" -Scope Global -ErrorAction SilentlyContinue
         $Edges = Get-NSXEdge | Select-Object id, name | Out-String
         Write-Output $Edges
         $RH = Read-Host "No NSX Edge selected, please select one from the list"
-        Set-Variable -Name "NSXEdge" -Value $RH -Scope Local -ErrorAction SilentlyContinue
+        Set-Variable -Name "NSXEdge" -Value $RH -Scope Global -ErrorAction SilentlyContinue
     }
 }
 
