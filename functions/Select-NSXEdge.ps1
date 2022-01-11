d6bfc6977e804fca35c31d3a01611d1d1267d28f
Mike Pruett <mikepruett3@gmail.com>
2022-01-11 16:22:43 -0600

    More updates to Select-NSXEdge function

--- functions/Select-NSXEdge.ps1
+++ functions/Select-NSXEdge.ps1
@@ -1,12 +1,16 @@
 
 function Select-NSXEdge {
-    param ()
-    if (!($NSXEdge)) {
-        Clear-Variable -Name "NSXEdge" -Scope Global -ErrorAction SilentlyContinue
-        $Edges = Get-NSXEdge | Select-Object id, name | Out-String
-        Write-Output $Edges
-        $RH = Read-Host "No NSX Edge selected, please select one from the list"
-        Set-Variable -Name "NSXEdge" -Value $RH -Scope Global -ErrorAction SilentlyContinue
-    }
+    # Clear previously set $NSXEdge Global Variable
+    Clear-Variable -Name "NSXEdge" -Scope Global -ErrorAction SilentlyContinue
+
+    # Query NSX for list of Edges (Global or Local)
+    $Edges = Get-NSXEdge | Select-Object id, name | Out-String
+    Write-Output $Edges
+
+    # User input of desired NSX Edge
+    $RH = Read-Host "No NSX Edge selected, please select one from the list"
+
+    # Set $NSXEdge Global Variable to new value
+    Set-Variable -Name "NSXEdge" -Value $RH -Scope Global -ErrorAction SilentlyContinue
 }
 
